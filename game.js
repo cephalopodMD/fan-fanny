140008ee0351c388b624971a928c76e47f4ec9b5
cephalopodMD <mistahowe@gmail.com>
2014-07-30 14:31:27 -0400

    Update game.js

--- game.js
+++ game.js
@@ -132,7 +132,7 @@ Particle.prototype.submitToHeat = function (sources, influence) {
 	// for each passed source
 	for (var i = 0; i < sources.length; i++) {
 		var dist = this.position.getDistanceFrom(sources[i].position);
-		if (dist > 0) {
+		if (dist > 1) {
 			this.temp += influence * (sources[i].temp - this.temp) / (dist);
 		}
 	}
