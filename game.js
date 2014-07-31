e217a68d1f8976e99d4e941709fb61d117172604
cephalopodMD <mistahowe@gmail.com>
2014-07-31 09:04:25 -0400

    Update game.js

--- game.js
+++ game.js
@@ -33,7 +33,7 @@ function plotParticles(boundsX, boundsY) {
 		
 		if (pos.x > canvas.width || pos.x < 0 || pos.y >canvas.height || pos.y < 0) {
 			var central = new Vector(320 - pos.x, 240 - pos.y);
-			central.scale(1/central.getMagnitude());
+			central.scale(particle.velocity.getmagnitude()/central.getMagnitude());
 			particle.acceleration.add(central);
 		}
 		
